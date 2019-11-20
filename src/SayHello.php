54bc63783bd22dcc42874e177f5359f3b2375c9e
Olya Nikolskaya <nikolskaya@tutu.tech>
2019-11-20 18:00:08 +0300

    fix

--- src/SayHello.php
+++ src/SayHello.php
@@ -1,9 +1,9 @@
 <?php
-namespace TraiTraining_project\Lib_for_lib;
+namespace Training_project\Lib_for_lib;
 
 class SayHello
 {
-    public static function sayHi($toSay = 'I have nothing to say')
+    public function sayHi($toSay = 'I have nothing to say')
     {
         return $toSay;
     }
